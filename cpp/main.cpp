1130e1743b7bc41c2a53328c9b7eb9cc19c94ccc
thim25 <thythim2004@gmail.com>
2024-02-08 09:10:48 +0700

    add cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 int main(){
 
-  cout<<"hello Tim";
-  
-
+  int array[]={10, 15, 18, 19};
+  int length = sizeof(array)/sizeof(array[0]);
+  cout <<"The length is : " << length << endl;
+  for (int i = 0; i < length; i++) {
+    if(array[i]%2==0){
+      cout << array[i] << "\n";
+    }
+  } 
   return 0;
 }
