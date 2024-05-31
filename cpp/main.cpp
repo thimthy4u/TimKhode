0642aa402f071f5f54b7c6d7937ba69e955fe53d
Tim <thythim2004@gmail.com>
2024-05-31 14:44:56 +0700

    update

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 using namespace std;
-int main(){
 
-  int array[]={10, 15, 18, 19};
-  int length = sizeof(array)/sizeof(array[0]);
-  cout <<"The length is : " << length << endl;
-  for (int i = 0; i < length; i++) {
-    if(array[i]%2==0){
-      cout << array[i] << "\n";
-    }
-  } 
+#include "timlibrary/loop.h"
+
+int main()
+{
+  loop();
+
   return 0;
 }
